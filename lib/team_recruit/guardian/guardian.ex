9c7dbb5254392b226be9f76eee993520b6c62390
qwexvf <qw3xvf@gmail.com>
2019-04-27 00:28:49 +0900

    updated guardian

--- lib/team_recruit/guardian/guardian.ex
+++ lib/team_recruit/guardian/guardian.ex
@@ -2,24 +2,16 @@ defmodule TeamRecruit.Guardian do
   use Guardian, otp_app: :team_recruit
   alias TeamRecruit.Accounts
 
-  def subject_for_token(%{"provider" => provider, "resource" => id}, _claims) do
-    # You can use any value for the subject of your token but
-    # it should be useful in retrieving the resource later, see
-    # how it being used on `resource_from_claims/1` function.
-    # A unique `id` is a good subject, a non-unique email address
-    # is a poor subject.
-    id = to_string(id)
-    {:ok, %{"provider" => provider, "resource" => id}}
+  def subject_for_token(user, _claims) do
+    id = to_string(user.id)
+    {:ok, id}
   end
   def subject_for_token(_, _) do
     {:error, :reason_for_error}
   end
 
-  def resource_from_claims(%{"sub" => sub}) do
-    # Here we'll look up our resource from the claims, the subject can be
-    # found in the `"sub"` key. In `above subject_for_token/2` we returned
-    # the resource id so here we'll rely on that to look it up.
-    user = Accounts.get_user!(sub["resource"])
+  def resource_from_claims(claims) do
+    user = Accounts.get_user!(claims["sub"])
     {:ok, user}
   end
   def resource_from_claims(_claims) do
