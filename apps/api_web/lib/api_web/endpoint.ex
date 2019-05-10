c63be9286133f4a9b6a43934104488725f46b7da
qwexvf <qw3xvf@gmail.com>
2019-05-10 14:10:16 +0900

    fix images path

--- apps/api_web/lib/api_web/endpoint.ex
+++ apps/api_web/lib/api_web/endpoint.ex
@@ -9,11 +9,11 @@ defmodule ApiWeb.Endpoint do
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :api,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+
+  plug Plug.Static, 
+    at: "/uploads", from: Path.expand("../../uploads"), gzip: false
+
+  IO.inspect Path.expand("../../uploads")
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
